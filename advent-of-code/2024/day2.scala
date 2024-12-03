f8efdd96433bfb003653c381a5c5405d63f5cd0e
James Collier <slakah@users.noreply.github.com>
2024-12-03 23:10:41 +0000

    solve problem 2

--- advent-of-code/2024/day2.scala
+++ advent-of-code/2024/day2.scala
@@ -6,24 +6,23 @@ import cats.syntax.all._
 
 val data = os.read.lines(os.pwd / "data" / "day2.dat")
 val reports = data.map(_.split("\\s+").map(_.toLong).toList)
-val deltaLines = reports.map(_
-    .sliding2
-    .map((x, y) => (x - y))
-  )
-println("Answer 1: " + deltaLines
+println("Answer 1: " + reports
+  .map(_.sliding2.map((x, y) => (x - y)))
   .count(deltas =>
     // Any two adjacent levels differ by at least one and at most three
     deltas.forall(delta => (1 to 3).contains(delta.abs))
     // The levels are either all increasing or all decreasing.
-    && (deltas.forall(_ > 0) || deltas.forall(_ <0))
-  )
-)
+    && (deltas.forall(_ > 0) || deltas.forall(_ < 0))
+  ))
 
-println("Answer 2: " + deltaLines
-  .count(deltas => {
-    // Any two adjacent levels differ by at least one and at most three
-    deltas.count(delta => (1 to 3).contains(delta.abs))
-    + (deltas.count(_))
-    val incrementing =
-  }
+println("Answer 2: " + reports
+  .map(line =>
+    (line :: (0 to line.length - 1).map(line.splitAt).map((left, right) => left ::: right.tail).toList)
+      .map(_.sliding2.map((x, y) => (x - y)))
   )
+  .count(_.exists(deltas =>
+    // Any two adjacent levels differ by at least one and at most three
+    deltas.forall(delta => (1 to 3).contains(delta.abs))
+    // The levels are either all increasing or all decreasing.
+    && (deltas.forall(_ > 0) || deltas.forall(_ < 0))
+  )))
