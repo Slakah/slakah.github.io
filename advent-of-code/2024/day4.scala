c17d52ec21f7cac88a0fa58d6f7d4a8cadd5002c
James Collier <Slakah@users.noreply.github.com>
2024-12-06 00:17:58 +0000

    Advent of code 2024 #4 (#119)
    * problem 1 solution
    
    * solve part 2

---
+++ advent-of-code/2024/day4.scala
@@ -0,0 +1,39 @@
+import $ivy.`com.lihaoyi::os-lib:0.9.2`
+import $ivy.`org.typelevel::cats-core:2.12.0`
+import cats._
+import cats.data._
+import cats.syntax.all._
+
+val data = os.read.lines(os.pwd / "data" / "day4.dat")
+val grid = (for
+  (line, row) <- data.zipWithIndex
+  (cell, column) <- line.zipWithIndex
+yield ((column, row), cell)).toMap
+val word = "XMAS"
+val compassPoints = for
+  column <- -1 to 1
+  row <- -1 to 1
+  if ((column, row) != (0, 0))
+yield (column, row)
+println("Answer 1: " + grid.map {
+  case ((column, row), 'X') =>
+    compassPoints.count((x, y) =>
+      word.tail.zipWithIndex.map((c, i) => (c, i + 1)).forall((c, i) =>
+        grid.get((column + (i * x), row + (i * y))) == Some(c)
+      )
+    )
+  case _ => 0
+}.sum)
+
+val ordinalPoints = List((1, 1), (1, -1), (-1, -1), (-1, 1))
+println("Answer 2: " + grid.count {
+  case ((column, row), 'A') =>
+    ordinalPoints.flatMap((x, y) => grid.get((column + x), (row + y))) match {
+      case List('M', 'M', 'S', 'S') => true
+      case List('S', 'M', 'M', 'S') => true
+      case List('S', 'S', 'M', 'M') => true
+      case List('M', 'S', 'S', 'M') => true
+      case _ => false
+    }
+  case _ => false
+})
