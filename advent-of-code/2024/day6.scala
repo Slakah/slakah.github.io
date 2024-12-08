38a9a814a793d5f344bf3253130a0efc2b63f619
James Collier <slakah@users.noreply.github.com>
2024-12-08 22:57:13 +0000

    solve problem 2

--- advent-of-code/2024/day6.scala
+++ advent-of-code/2024/day6.scala
@@ -1,5 +1,6 @@
 import $ivy.`com.lihaoyi::os-lib:0.9.2`
 import $ivy.`org.typelevel::cats-core:2.12.0`
+import scala.annotation.tailrec
 import cats._
 import cats.data._
 import cats.syntax.all._
@@ -9,30 +10,58 @@ val grid = (for
   (line, row) <- data.reverse.zipWithIndex
   (cell, column) <- line.zipWithIndex
 yield ((column + 1, row), cell)).toMap
-var path = List.empty[(Int, Int)]
-var (x, y) = grid.find(_._2 == '^').get._1
-var direction: '^' | '>' | 'v' | '<'  = '^'
+var visited = List.empty[(Int, Int)]
+val startingPosition = grid.find(_._2 == '^').get._1
+type Direction = '^' | '>' | 'v' | '<'
+type Path = List[((Int, Int), Direction)]
+type Grid = Map[(Int, Int), Char]
+var direction: Direction  = '^'
 
-while (grid.contains((x, y))) {
-  visited = (x, y) :: visited
-  val (newX, newY) = direction match {
+def rotate90(direction: Direction): Direction = direction match {
+  case '^' => '>'
+  case '>' => 'v'
+  case 'v' => '<'
+  case '<' => '^'
+}
+
+def forward(position: (Int, Int), direction: Direction): (Int, Int) = {
+  val (x, y) = position
+  direction match {
     case '^' => (x, y + 1)
     case '>' => (x + 1, y)
     case 'v' => (x, y - 1)
     case '<' => (x - 1, y)
   }
-  grid.get((newX, newY)) match {
-    case Some('#') =>
-      direction = (direction match {
-        case '^' => '>'
-        case '>' => 'v'
-        case 'v' => '<'
-        case '<' => '^'
-      })
-    case _ =>
-      x = newX
-      y = newY
-  }
 }
 
-println("Answer 1: " + visited.distinct.length)
+@tailrec
+def walk(path: Path, theGrid: Grid = grid, depth: Int = 0): (Path, Boolean) = {
+  val segment = path.head
+  if (depth % 500 == 0 && path.tail.contains(segment)) {
+    // loop detected
+    // loop detection is slow, so only run when loop is more likely
+    (path, true)
+  } else {
+    val ((x, y), direction: Direction) = segment
+    val (newX, newY) = forward((x, y), direction)
+    theGrid.get((newX, newY)) match {
+      case Some('#') =>
+        val newDirection: Direction = rotate90(direction)
+        walk(((x, y), newDirection) :: path, theGrid, depth + 1)
+      case Some(_) =>
+        walk(((newX, newY), direction) :: path, theGrid, depth + 1)
+      case None => (path, false)
+    }
+  }
+}
+val path = walk(List((startingPosition, direction)))._1
+println("Answer 1: " + path.map(_._1).distinct.length)
+
+println("Answer 2: " + path.reverse.foldLeft((List.empty[(Int, Int)], List.empty[(Int, Int)])) {
+  case ((visited, loopObstacles), (position, direction: Direction)) =>
+    val obstaclePos = forward(position, direction)
+    if (!visited.contains(obstaclePos) && walk(List((position, direction)), grid + (obstaclePos -> '#'))._2) {
+      (position :: visited, obstaclePos :: loopObstacles)
+    } else (position :: visited, loopObstacles)
+  }._2.distinct.length
+)
