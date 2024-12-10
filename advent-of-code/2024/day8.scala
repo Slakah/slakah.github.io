a9c164db7370189468195d2cf9d6776235714246
James Collier <Slakah@users.noreply.github.com>
2024-12-10 05:40:08 -0500

    Advent of code 2024 #8 (#123)
    * solve day 8 problem 1
    
    * partial for problem 2
    
    * solved day 8

---
+++ advent-of-code/2024/day8.scala
@@ -0,0 +1,42 @@
+import $ivy.`com.lihaoyi::os-lib:0.9.2`
+import $ivy.`org.typelevel::cats-core:2.12.0`
+import cats._
+import cats.data._
+import cats.syntax.all._
+
+val data = os.read.lines(os.pwd / "data" / "day8.dat")
+val grid = (for
+  (line, y) <- data.reverse.zipWithIndex
+  (cell, x) <- line.zipWithIndex
+  if cell != '.'
+yield ((x, y), cell))
+
+val (width, height) = (data.map(_.length).max, data.length)
+val maxWidthHeight = List(width, height).max
+
+val freq2coords = grid.groupBy(_._2).view.mapValues(_.map(_._1).toList).toList
+
+println("Answer 1: " + freq2coords.flatMap((freq, coords) => 
+  coords.combinations(2).toList.flatMap {
+    case (x1, y1) :: (x2, y2) :: Nil =>
+      val (deltaX, deltaY) = (x2 - x1, y2 - y1)
+      List((x1 - deltaX, y1 - deltaY), (x2 + deltaX, y2 + deltaY))
+    case _ => ???
+  })
+  .distinct
+  .count((x, y) => x >= 0 && x < width && y >= 0 && y < height)
+)
+
+
+println("Answer 2: " + freq2coords.flatMap((freq, coords) => 
+  coords.combinations(2).toList.flatMap {
+    case (x1, y1) :: (x2, y2) :: Nil =>
+      val (deltaX, deltaY) = (x2 - x1, y2 - y1)
+      // TODO: optimization to only generate on the grid
+      (-maxWidthHeight to maxWidthHeight)
+        .flatMap(factor => List(((x2 + deltaX * factor), (y2 + deltaY * factor))))
+    case _ => ???
+  })
+  .distinct
+  .count((x, y) => x >= 0 && x < width && y >= 0 && y < height)
+)
