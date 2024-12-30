d676ec19584882fec68153e04f663969f0fbdad4
James Collier <Slakah@users.noreply.github.com>
2024-12-30 21:44:53 +0000

    update

--- dash/bin/chromium-screenshot.sh
+++ dash/bin/chromium-screenshot.sh
@@ -3,5 +3,11 @@ set -euo pipefail
 
 readonly url="$1"
 readonly output="$2"
-echo "taking screenshot of $1 to $2..."
+
+readonly response_status=$(curl -o /dev/null -s -w "%{http_code}" "$url")
+if [ "200" -ne "$response_status" ]; then
+    echo "got $response_status from $url"
+fi
+
+echo "taking screenshot of $url to $output..."
 exec chromium-browser --headless --no-sandbox --disable-gpu --hide-scrollbars --screenshot=$output --window-size=400x300 $url
