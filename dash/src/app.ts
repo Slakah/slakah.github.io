6979018f30765fcb20cd1a76e191640d06634670
James Collier <Slakah@users.noreply.github.com>
2024-12-30 01:35:46 +0000

    use public stuff from source

--- dash/src/app.ts
+++ dash/src/app.ts
@@ -4,11 +4,7 @@ import path from 'path';
 const app = express();
 const port = process.env.PORT || 3000;
 
-// app.get('/', (req: Request, res: Response) => {
-//   res.send('Hello, TypeScript!');
-// });
-
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '..', 'public')));
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
